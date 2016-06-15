06eb475e53cd9cd42f20ec03a68ffe1401012f4c
Leonardo Fachetti <lfachetti@uolinc.com>
2016-06-15 14:47:47 -0300

    Adicionando log da porta em que o server está rodando

--- app.js
+++ app.js
@@ -5,4 +5,6 @@ var open = require('open');
 app.use(serverStactic('app'));
 
 open('http://localhost:7000');
-app.listen(7000);
+app.listen(7000, function() {
+  console.log('Listening in port 7000');
+});
