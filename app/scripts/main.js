d1dbf054942744dd3712ada2168e36932a03ed2a
Vander <vandersilvagil@gmail.com>
2018-04-26 22:46:09 -0300

    Atualizacao Código

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,96 +1,66 @@
 $(document).ready(function () {
-   
+
     getJsonData('');
 
     // Busca no arquivo por nome ou email quando aperta as teclas no input de busca
-    $("#buscar").keyup(function( event ) {
-       var a = $("#buscar").val();
-       getJsonData(a);
+    $("#buscar").keyup(function (event) {
+        var a = $("#buscar").val();
+        getJsonData(a);
     });
 
     // Limpa a caixa de busca
-    $("#botao-limpar").click(function() {
+    $("#botao-limpar").click(function () {
         getJsonData('');
         $("#buscar").val('');
     });
 
     // Remove o placeholder quando esta em foco
-    $('input').focus(function(){
-        $(this).data('placeholder', $(this).attr('placeholder'))
-               .attr('placeholder','');
-     }).blur(function(){
+    $('input').focus(function () {
+        $(this).data('placeholder', $(this).attr('placeholder')).attr('placeholder', '');
+    }).blur(function () {
         $(this).attr('placeholder', $(this).data('placeholder'));
-     });
-
-
+    });
 });
 
+// Busca dados do arquivo Json
+function getJsonData(busca) {
 
-$(document).ready(function () {
-   
-    getJsonData('');
-
-    // Busca no arquivo por nome ou email quando aperta as teclas no input de busca
-    $("#buscar").keyup(function( event ) {
-       var a = $("#buscar").val();
-       getJsonData(a);
-    });
-
-    // Limpa a caixa de busca
-    $("#botao-limpar").click(function() {
-        getJsonData('');
-        $("#buscar").val('');
-    });
-
-    // Remove o placeholder quando esta em foco
-    $('input').focus(function(){
-        $(this).data('placeholder', $(this).attr('placeholder'))
-               .attr('placeholder','');
-     }).blur(function(){
-        $(this).attr('placeholder', $(this).data('placeholder'));
-     });
-
-
-});
-
-
-function getJsonData(busca){
-
-    function sortJsonName(a,b){
-	    return a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1;
-	};
+    //Ordena dados do json por nome
+    function sortJsonName(a, b) {
+        return a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1;
+    };
 
     $.getJSON('users.json', function (data) {
 
         data = $(data).sort(sortJsonName);
 
-        var cli_dados = '';       
-        
-        $.each(data, function(key, value) {
+        var cli_dados = '';
+
+        $.each(data, function (key, value) {
 
             var nome = value.name;
             var email = value.contact;
             var tipoStatus = value.status.type;
             var descricaoStatus = value.status.description;
-            
-            if(nome.toLowerCase().indexOf(busca.toLowerCase()) >= 0 ||
-               email.toLowerCase().indexOf(busca.toLowerCase()) >= 0 || 
-               busca == ''){
 
-                cli_dados += '<div class="result">';             
+            if (nome.toLowerCase().indexOf(busca.toLowerCase()) >= 0 ||
+                email.toLowerCase().indexOf(busca.toLowerCase()) >= 0 ||
+                busca == '') {
+
+                cli_dados += '<div class="result">';
                 cli_dados += '<div id="foto-perfil"><img src="images/foto-perfil.png" alt="Foto Perfil"></div>';
-                
+
                 cli_dados += '    <div id="nome-contato">';
 
-                if(tipoStatus == 'WAITING' || tipoStatus == 'WAITING-ACCEPTED'){
+                if (tipoStatus == 'WAITING' || tipoStatus == 'WAITING-ACCEPTED') {
                     cli_dados += '      <p id="nome" style="color:#999999">' + nome + '</p>';
                     cli_dados += '      <p id="contato" style="color:#999999">' + email + '</p>';
-                }else{
+                } else {
                     cli_dados += '      <p id="nome" style="color:#1082be">' + nome + '</p>';
                     cli_dados += '      <p id="contato"  style="color:#484848">' + email + '</p>';
                 }
                 cli_dados += '    </div>';
-                
+
                 cli_dados += getImage(tipoStatus, descricaoStatus);
                 cli_dados += '    <div id="config"><img src="images/config.jpg" alt="Config"></div>';
                 cli_dados += '</div>';
@@ -101,11 +71,12 @@ function getJsonData(busca){
     });
 }
 
-function getImage(type, description){
+// Busca imagem de acordo com o tipo da descrição do status
+function getImage(type, description) {
 
     var color;
 
-    switch(type){
+    switch (type) {
         case 'ACTIVE':
             color = 'verde';
             break;
@@ -119,36 +90,6 @@ function getImage(type, description){
             color = 'preto';
             break;
     }
-
-
-    return '<div id="painel">' +
-                '<p><img src="images/status-' + color + '.png" alt="Painel Compartilhado">' + description + '</p>'+
-            '</div>';
-
-}
-
-function getImage(type, description){
-
-    var color;
-
-    switch(type){
-        case 'ACTIVE':
-            color = 'verde';
-            break;
-        case 'WAITING':
-            color = 'amarelo';
-            break;
-        case 'EXIPIRED':
-            color = 'vermelho';
-            break;
-        case 'WAITING-ACCEPTED':
-            color = 'preto';
-            break;
-    }
-
-
-    return '<div id="painel">' +
-                '<p><img src="images/status-' + color + '.png" alt="Painel Compartilhado">' + description + '</p>'+
-            '</div>';
+    return '<div id="painel"><p><img src="images/status-' + color + '.png" alt="Painel Compartilhado">' + description + '</p></div>';
 
 }
