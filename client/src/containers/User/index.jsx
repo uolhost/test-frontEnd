69a8529860a865a73e5e21c926e08944c6eb05bd
Esdras Rios Baia <esdrasrioos@gmail.com>
2018-05-03 09:26:50 -0300

    add show only 5 results

--- client/src/containers/User/index.jsx
+++ client/src/containers/User/index.jsx
@@ -3,48 +3,62 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import UserResults from '../../components/UserResults/index';
 import SearchBox from '../SearchBox/index';
+import Pagination from '../Pagination';
 
 class User extends Component {
   state = {
     users: [],
     filteredUsers: [],
+    maxUsers: [],
+    maxPages: '',
+    usersPerPage: 5,
     currentPage: 1,
   }
 
   componentDidMount() {
     axios.get('http://localhost:3000/users')
       .then((res) => {
-        const users = res.data;
-        users.sort((a, b) => {
-          const nameA = a.name.toUpperCase();
-          const nameB = b.name.toUpperCase();
-          if (nameA < nameB) {
-            return -1;
-          }
-          if (nameA > nameB) {
-            return 1;
-          }
-          return 0;
-        });
+        let users = res.data;
+        users = _.orderBy(users, 'name', 'asc');
         this.setState({ users });
-        this.setState({ filteredUsers: users });
-        console.log(this.state.filteredUsers)
+        this.filterUserFromSearch('');
       });
   }
 
   filterUserFromSearch = (searchTerm) => {
-    let filteredUsers = this.state.users;
-    filteredUsers = filteredUsers
-      .filter(user => user.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-      user.contact.toLowerCase().includes(searchTerm.toLowerCase()));
-    this.setState({ filteredUsers });
+    const filteredUsers = this.state.users
+      .filter(user => user.name.toLowerCase()
+        .includes(searchTerm.toLowerCase()) ||
+        user.contact.toLowerCase().includes(searchTerm.toLowerCase()));
+    const sliced = filteredUsers.slice(0, 5);
+    this.maxUsersPerPages(sliced);
   };
+
+
+  maxUsersPerPages(arr) {
+    const { usersPerPage, currentPage, filteredUsers } = this.state;
+    const lastUserIndex = currentPage * usersPerPage;
+    const firstUserIndex = lastUserIndex - usersPerPage;
+    const maxUser = arr.slice(firstUserIndex, lastUserIndex);
+    this.setState({ filteredUsers: maxUser });
+  }
+
+  pageNum() {
+    const { usersPerPage, currentPage, users } = this.state;
+    const pageNum = [];
+    for (let i = 1; i <= Math.ceil(users.length / usersPerPage); i++) {
+      pageNum.push(i);
+    }
+    this.setState({ maxPages: pageNum })
+  }
+
 
   render() {
     return (
       <div>
         <SearchBox filterUser={this.filterUserFromSearch} />
-        <UserResults userInfo={this.state} />
+        <UserResults userInfo={this.state.filteredUsers} />
+        <Pagination handlePages={this.handlePagesNum} />
       </div>
     );
   }
