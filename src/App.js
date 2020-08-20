fc2ca52c20952aae92fb2cc734d98c15ee781dcf
Gabriel <knuckles4x@gmail.com>
2020-08-20 04:37:13 -0300

    global styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import GlobalStyles from "./styles/GlobalStyles";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default App;
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  html {
+    min-height: 100%;
+    background: var(--white);
+  }
+  *, button, input {
+    border: 0;
+    background: none;
+    font-family: Lato;
+    color: var(--primary);
+  }
+  ul {
+    list-style: none
+  }
+  :root {
+    --white: #fff;
+    --border: #EAEAEA;
+    --primary: #333;
+    --secondary: #47525E;
+    --tertiary: #666;
+
+    --active: #49AD5B;
+    --inactive: #D6323F;
+    --waiting: #D2A710;
+    --disable: #D2D2D2;
+
+    --main-color: #E29933;
+  }
+`;
